158e91ee112a08999b2a2800ae20989335ced4e1
Franck Achkouyan <achko.franck@gmail.com>
2019-01-03 15:26:46 +0100

    migrated to nextjs 8 canary serverless, with bumps & robot.txt

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
 const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
     ? {} // We're never in "production server" phase when in development mode
-    : !process.env.NOW_REGION 
-      ? require('next/constants') // Get values from `next` package when building locally
-      : require('next-server/constants'); // Get values from `next-server` package when building on now v2
+    : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants') // Get values from `next-server` package when building on now v2
+
+const nextJsConfig = {
+  target: 'serverless',
+}
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
     // Config used to run in production.
-    return {};
+    return nextJsConfig
   }
 
   const withMDX = require('@zeit/next-mdx')({
     extension: /.mdx?$/,
   })
 
-  return withMDX();
+  return withMDX(nextJsConfig)
 }
