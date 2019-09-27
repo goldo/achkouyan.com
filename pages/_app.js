12e7ff4f260a1ae97128197de50a3e09e7c707c9
Franck Achkouyan <achko.franck@gmail.com>
2019-09-28 00:49:48 +0200

    chore(bump): bump everything

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import Head from 'next/head'
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
@@ -82,7 +82,7 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Container>
+      <>
         <GlobalStyle />
         <Head>
           <title>Franck Achkouyan | Fullstack JS Developer</title>
@@ -94,7 +94,7 @@ export default class MyApp extends App {
           </Content>
           <Footer />
         </Main>
-      </Container>
+      </>
     )
   }
 }
